8fc2d32d2ef6ed5efc951061a01601d8703eb08f
brkfce <calsutton12@gmail.com>
2022-08-24 23:33:02 +0100

    Created and initialised game struct.

--- src/game.rs
+++ src/game.rs
@@ -1,2 +1,28 @@
 pub mod board;
 pub mod pieces;
+
+pub struct Game {
+    board: board::Board,
+    pieces: pieces::Pieces,
+}
+
+#[allow(dead_code)]
+pub fn new_game() -> Game {
+    let pieces_set = create_pieces();
+    let game_board = create_board();
+    let new_game = Game {
+        board: game_board,
+        pieces: pieces_set,
+    };
+    new_game
+}
+
+fn create_pieces() -> pieces::Pieces {
+    let pieces_set = pieces::Pieces {};
+    pieces_set
+}
+
+fn create_board() -> board::Board {
+    let game_board = board::Board {};
+    game_board
+}
