726129c589ff5d09723d9f76d809d2ec8aadc565
brkfce <calsutton12@gmail.com>
2022-08-25 00:37:24 +0100

    Moved board and pieces creation funcs to their mod

--- src/game.rs
+++ src/game.rs
@@ -8,21 +8,11 @@ pub struct Game {
 
 #[allow(dead_code)]
 pub fn new_game() -> Game {
-    let pieces_set = create_pieces();
-    let game_board = create_board();
+    let pieces_set = pieces::create_pieces();
+    let game_board = board::create_board();
     let new_game = Game {
         board: game_board,
         pieces: pieces_set,
     };
     new_game
-}
-
-fn create_pieces() -> pieces::Pieces {
-    let pieces_set = pieces::Pieces {};
-    pieces_set
-}
-
-fn create_board() -> board::Board {
-    let game_board = board::Board {};
-    game_board
 }
