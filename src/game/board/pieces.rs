cfe8954ed6daa144ee4665e8b13cb6dc1f7864eb
brkfce <calsutton12@gmail.com>
2022-08-25 18:12:43 +0100

    Created struct for each type of  piece.

--- src/game/board/pieces.rs
+++ src/game/board/pieces.rs
@@ -1,1 +1,23 @@
-mod pieces {}
+#[allow(dead_code)]
+
+mod pieces {
+
+    struct Pawn {
+        position_index: i8,
+    }
+    struct Knight {
+        position_index: i8,
+    }
+    struct Bishop {
+        position_index: i8,
+    }
+    struct Rook {
+        position_index: i8,
+    }
+    struct Queen {
+        position_index: i8,
+    }
+    struct King {
+        position_index: i8,
+    }
+}
