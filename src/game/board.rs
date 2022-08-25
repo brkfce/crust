a5960bb7d067a1b6f8ccb226e07da48d7c6f3643
brkfce <calsutton12@gmail.com>
2022-08-26 00:05:09 +0100

    Added test for initial pawn moves.

--- src/game/board.rs
+++ src/game/board.rs
@@ -301,3 +301,28 @@ pub fn gen_moves(board: &Board) {
         vec_pos += 1;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{gen_black_positions, gen_white_position, pieces::GenMoves};
+
+    #[test]
+    fn one_white_pawn_moves() {
+        let board = super::new_board();
+        let mut moves_list: Vec<super::Board> = Vec::new();
+        let white_positions = gen_white_position(&board);
+        let black_positions = gen_black_positions(&board);
+        board.white_pawns[0].gen_moves(
+            0,
+            super::Colour::White,
+            &board,
+            white_positions,
+            black_positions,
+            &mut moves_list,
+        );
+        let move_one_index = moves_list[0].white_pawns[0].position_index;
+        let move_two_index = moves_list[1].white_pawns[0].position_index;
+        assert_eq!(move_one_index, 16);
+        assert_eq!(move_two_index, 24);
+    }
+}
