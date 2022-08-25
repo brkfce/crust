4299a812172a4cc3baca591427cf99d14acc0e8e
brkfce <calsutton12@gmail.com>
2022-08-25 18:33:08 +0100

    Created board struct to store pieces and game data

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,3 +1,29 @@
+#[allow(dead_code)]
 mod pieces;
 
-mod board {}
+enum Pieces {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+enum Turn {
+    White = 1,
+    Black = -1,
+}
+
+pub struct Board {
+    white_pieces: Vec<Pieces>,
+    black_pieces: Vec<Pieces>,
+    turn: Turn,
+    move_count: i32,
+    enpassant: bool,
+    white_castle_kingside: bool,
+    white_castle_queenside: bool,
+    black_castle_kingside: bool,
+    black_castle_queenside: bool,
+}
+
+pub fn new_board() -> Board {}
