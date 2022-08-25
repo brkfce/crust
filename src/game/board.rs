3e0124b4da424eb4820272d746caa362f85c7405
brkfce <calsutton12@gmail.com>
2022-08-25 19:39:23 +0100

    Changed board to store vec of each piece type.

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 #[allow(dead_code)]
 mod pieces;
 
@@ -15,9 +17,18 @@ enum Turn {
 }
 
 pub struct Board {
-    white_pieces: Vec<Pieces>,
-    black_pieces: Vec<Pieces>,
-    turn: Turn,
+    white_pawns: Vec<pieces::Pawn>,
+    white_knights: Vec<pieces::Knight>,
+    white_bishops: Vec<pieces::Bishop>,
+    white_rooks: Vec<pieces::Rook>,
+    white_queens: Vec<pieces::Queen>,
+    white_king: pieces::King,
+    black_pawns: Vec<pieces::Pawn>,
+    black_knights: Vec<pieces::Knight>,
+    black_bishops: Vec<pieces::Bishop>,
+    black_rooks: Vec<pieces::Rook>,
+    black_queens: Vec<pieces::Queen>,
+    black_king: pieces::King,
     move_count: i32,
     enpassant: bool,
     white_castle_kingside: bool,
@@ -26,4 +37,18 @@ pub struct Board {
     black_castle_queenside: bool,
 }
 
-pub fn new_board() -> Board {}
+pub fn new_board() -> Board {
+    Board {
+        white_pawns: vec![
+            pieces::Pawn { position_index: 8 },
+            pieces::Pawn { position_index: 9 },
+            pieces::Pawn { position_index: 10 },
+            pieces::Pawn { position_index: 11 },
+            pieces::Pawn { position_index: 12 },
+            pieces::Pawn { position_index: 13 },
+            pieces::Pawn { position_index: 14 },
+            pieces::Pawn { position_index: 15 },
+        ],
+        white_knights: vec![pieces::],
+    }
+}
